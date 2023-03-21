78c7589aebb75094b7b054e6a96eef9d25e0012b
prao <pch5521@gmail.com>
2023-03-21 14:34:06 +0900

    jpa 테이블 설계

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -17,15 +17,30 @@ public class JpaMain {
 
         try {
 
-            // 영속
-            Member member = em.find(Member.class, 150L);
-            member.setName("AAAAA");
+            Member member1 = new Member();
+            member1.setUsername("A");
 
-            em.clear();
+            Member member2 = new Member();
+            member2.setUsername("B");
 
-            Member member2 = em.find(Member.class, 150L);
+            Member member3 = new Member();
+            member3.setUsername("C");
 
-            System.out.println("===========================");
+            System.out.println("===========");
+
+            // DB SEQ = 1       |   1
+            // DB SEQ = 51      |   2
+            // DB SEQ = 51      |   3
+
+            em.persist(member1);    // 1, 51
+            em.persist(member2);  // MEMORY
+            em.persist(member3);  // MEMORY
+
+            System.out.println("member1 = " + member1.getId());
+            System.out.println("member2 = " + member2.getId());
+            System.out.println("member3 = " + member3.getId());
+            System.out.println("===========");
+
             tx.commit(); // commit한 시점에 영속성 컨텍스트에서 DB로 날아감
         } catch(Exception e) {
             tx.rollback();
--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -1,22 +1,24 @@
 package hellojpa;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
+
 
 @Entity
+@SequenceGenerator(
+        name = "MEMBER_SEQ_GENERATOR",
+        sequenceName = "MEMBER_SEQ", // 매핑할 데이터베이스 시퀀스 이름
+        initialValue = 1, allocationSize = 50)
 public class Member {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE,
+            generator = "MEMBER_SEQ_GENERATOR")
     private Long id;
-    private String name;
-    private int gogo;
-    public Member() {
 
-    }
-    public Member(Long id, String name) {
-        this.id = id;
-        this.name = name;
+    @Column(name = "name", nullable = false)      // db -> name
+    private String username;    // 객체 -> username
+
+    public Member() {
     }
 
     public Long getId() {
@@ -27,11 +29,11 @@ public class Member {
         this.id = id;
     }
 
-    public String getName() {
-        return name;
+    public String getUsername() {
+        return username;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setUsername(String username) {
+        this.username = username;
     }
 }
